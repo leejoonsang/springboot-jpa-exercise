6b78d1be260e12efe47e24842ec3d2f20b531ab0
Sangjoon <djm04212@naver.com>
2022-11-17 13:30:50 +0900

    Initial Push

---
+++ src/main/java/com/example/springbootjpa/domain/entity/User.java
@@ -0,0 +1,21 @@
+package com.example.springbootjpa.domain.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Builder
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String password;
+}
